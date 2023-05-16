5009f336256f9d73facdb123f87be75bfe4c67d2
yepianer <aimeinvshen@126.com>
2023-05-16 11:57:40 +0800

    publisher-comfirm:ack nack returnCallback

--- publisher/src/main/java/cn/itcast/mq/config/CommonConfig.java
+++ publisher/src/main/java/cn/itcast/mq/config/CommonConfig.java
@@ -1,11 +1,30 @@
 package cn.itcast.mq.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
-public class CommonConfig{
+public class CommonConfig implements ApplicationContextAware {
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        //获取RabbitTemplate对象
+        RabbitTemplate rabbitTemplate = applicationContext.getBean(RabbitTemplate.class);
+        //配置ReturnCallback
+        rabbitTemplate.setReturnCallback((Message message, int replyCode, String replyText, String exchange, String routingKey) ->{
+            // 记录日志
+            log.error("消息发送队列失败，响应码:{},失败原因:{},交换机:{},路由key:{},消息:{}",
+            replyCode,replyText,exchange,routingKey,message.toString());
+            // 如果有需要的话，重发消息
+        });
+    }
 }
--- publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -3,10 +3,15 @@ package cn.itcast.mq.spring;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.SuccessCallback;
+
+import java.util.UUID;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -17,8 +22,28 @@ public class SpringAmqpTest {
 
     @Test
     public void testSendMessage2SimpleQueue() throws InterruptedException {
+        //1.准备消息
         String routingKey = "simple";
+        //2.准备correlationData
+        //2.1 消息ID
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        //2.2 准备
+        correlationData.getFuture().addCallback(result -> {
+            //判断结果
+            if (result.isAck()){
+                //ACK
+                log.debug("消息成功投递到交换机!消息ID:{}",correlationData.getId());
+            }else {
+                //NACK
+                log.error("消息投递到交换机失败！消息ID:{}",correlationData.getId());
+            }
+        }, ex -> {
+            // 记录日志
+            log.error("消息发送失败！",ex);
+            // 重发消息
+        });
+        //3.发送消息
         String message = "hello, spring amqp!";
-        rabbitTemplate.convertAndSend("camq.topic", routingKey, message);
+        rabbitTemplate.convertAndSend("amq.topic", "simple.test", message,correlationData);
     }
 }
